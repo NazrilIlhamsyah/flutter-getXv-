4bf31e29add6ebe60a956537ff12225b285c2336
Rydwan108 <muhammadrydwan17@gmail.com>
2023-02-24 14:25:01 +0700

    awal

---
+++ android/app/src/main/kotlin/com/project/ujikom/flutter_get/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.project.ujikom.flutter_get
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/app/modules/dashboard/controllers/dashboard_controller.dart
@@ -0,0 +1,20 @@
+import 'package:get/get.dart';
+
+class DashboardController extends GetxController {
+  final _getConnect = GetConnect();
+  //TODO: Implement DashboardController
+
+  
+  @override
+  void onInit() {
+    super.onInit();
+  }
+
+  @override
+  void onReady() {
+    super.onReady();
+  }
+
+  @override
+  void onClose() {}
+}
